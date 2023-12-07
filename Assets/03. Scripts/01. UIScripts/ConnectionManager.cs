74cdf95be41f4f684724091b66976dcf3c20bfca
Festison <sundogdog1958@gmail.com>
2023-12-07 17:10:02 +0900

    UI 업데이트 (12-07 이정철)

--- Assets/03. Scripts/01. UIScripts/ConnectionManager.cs
+++ Assets/03. Scripts/01. UIScripts/ConnectionManager.cs
@@ -17,13 +17,12 @@ public class ConnectionManager : MonoBehaviourPunCallbacks
     {
         // 마스터 클라이언트는 PhotonNetwork.LoadLevel()를 호출할 수 있고, 모든 연결된 플레이어는 자동적으로 동일한 레벨을 로드한다.
         PhotonNetwork.AutomaticallySyncScene = true;
-        
+
         m_panel_Loading.SetActive(false);
     }
 
     void Start()
     {
-
         print("서버 연결 시도.");
         PhotonNetwork.ConnectUsingSettings();
     }
@@ -32,23 +31,32 @@ public class ConnectionManager : MonoBehaviourPunCallbacks
     {
         string nick = m_inputField_Nickname.text;
 
-        print($"{nick} 랜덤 매칭 시작.");
-        PhotonNetwork.LocalPlayer.NickName = nick; // 현재 플레이어 닉네임 설정하기.
+        if (nick.Length>0)
+        {
+            print($"{nick} 랜덤 매칭 시작.");
+            PhotonNetwork.LocalPlayer.NickName = nick; // 현재 플레이어 닉네임 설정하기.
 
-        // UI에서 값 얻어오기.
-        byte maxPlayers = byte.Parse(m_dropdown_RoomMaxPlayers.options[m_dropdown_RoomMaxPlayers.value].text); // 드롭다운에서 값 얻어오기.
-        int maxTime = int.Parse(m_dropdown_MaxTime.options[m_dropdown_MaxTime.value].text);
+            // UI에서 값 얻어오기.
+            byte maxPlayers = byte.Parse(m_dropdown_RoomMaxPlayers.options[m_dropdown_RoomMaxPlayers.value].text); // 드롭다운에서 값 얻어오기.
+            int maxTime = int.Parse(m_dropdown_MaxTime.options[m_dropdown_MaxTime.value].text);
 
-        RoomOptions roomOptions = new RoomOptions();
-        roomOptions.MaxPlayers = maxPlayers; // 인원 지정.
-        roomOptions.CustomRoomProperties = new ExitGames.Client.Photon.Hashtable() { { "maxTime", maxTime } }; // 게임 시간 지정.
-        roomOptions.CustomRoomPropertiesForLobby = new string[] { "maxTime" }; // 여기에 키 값을 등록해야, 필터링이 가능하다.
+            RoomOptions roomOptions = new RoomOptions();
+            roomOptions.MaxPlayers = maxPlayers; // 인원 지정.
+            roomOptions.CustomRoomProperties = new ExitGames.Client.Photon.Hashtable() { { "maxTime", maxTime } }; // 게임 시간 지정.
+            roomOptions.CustomRoomPropertiesForLobby = new string[] { "maxTime" }; // 여기에 키 값을 등록해야, 필터링이 가능하다.
 
-        // 방 참가를 시도하고, 실패하면 생성해서 참가함.
-        PhotonNetwork.JoinRandomOrCreateRoom(
-            expectedCustomRoomProperties: new ExitGames.Client.Photon.Hashtable() { { "maxTime", maxTime } }, expectedMaxPlayers: maxPlayers, // 참가할 때의 기준.
-            roomOptions: roomOptions // 생성할 때의 기준.
-        );
+            // 방 참가를 시도하고, 실패하면 생성해서 참가함.
+            PhotonNetwork.JoinRandomOrCreateRoom
+            (
+                expectedCustomRoomProperties: new ExitGames.Client.Photon.Hashtable() { { "maxTime", maxTime } },
+                expectedMaxPlayers: maxPlayers, // 참가할 때의 기준.
+                roomOptions: roomOptions // 생성할 때의 기준.
+            );
+        }
+        else
+        {
+            Debug.Log("닉네임을 설정하세요");
+        }
     }
 
     public void CancelMatching()
