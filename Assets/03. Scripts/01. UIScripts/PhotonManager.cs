29468d08a63b2fe1c3238898c0ddb5c27ece84c0
staple91 <24953523+staple91@users.noreply.github.com>
2023-12-16 19:36:27 +0900

    키보드 인식 오류 수정

--- Assets/03. Scripts/01. UIScripts/PhotonManager.cs
+++ Assets/03. Scripts/01. UIScripts/PhotonManager.cs
@@ -109,7 +109,7 @@ namespace LeeJungChul
 
             if (PhotonNetwork.CurrentRoom.PlayerCount == 1 && PhotonNetwork.CurrentRoom.MaxPlayers == 1)
             {
-                PhotonNetwork.LoadLevel("JCIscream");
+                PhotonNetwork.LoadLevel("Iscream");
             }
         }
 
@@ -124,7 +124,7 @@ namespace LeeJungChul
 
                 if (PhotonNetwork.CurrentRoom.PlayerCount == PhotonNetwork.CurrentRoom.MaxPlayers)
                 {
-                    PhotonNetwork.LoadLevel("JCIscream");
+                    PhotonNetwork.LoadLevel("Iscream");
                 }
             }
         }
