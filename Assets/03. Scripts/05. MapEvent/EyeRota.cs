98fa2b153fea43c9a0e539f612dd729c672e350e
PangGom <88571548+PangGom@users.noreply.github.com>
2023-12-17 05:26:13 +0900

    눈알로테이션(수정중)
    오류 난 부분은 일단 주석처리하여 올려두기만 하였습니다.

---
+++ Assets/03. Scripts/05. MapEvent/EyeRota.cs
@@ -0,0 +1,33 @@
+using KimKyeongHun;
+using No;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.UI.GridLayoutGroup;
+
+namespace PangGom
+{
+    public class EyeRota : MonoBehaviour
+    {
+        public GameObject target;
+
+        Player owner;
+        public Player Owner
+        {
+            get => owner;
+            set
+            {
+                owner = value;
+            }
+        }
+        void Start () 
+        {
+            //target = Owner.GetComponent<GameObject>();
+        }
+        void Update()
+        {
+            //Vector3 vector = target.transform.position - transform.position;
+            //transform.rotation = Quaternion.LookRotation(vector).normalized;
+        }
+    }
+}
