df13ea6d020dbaa909a5ff05ef3a5013f1a84534
Festison <sundogdog1958@gmail.com>
2023-12-19 00:55:33 +0900

    상호작용 이벤트 (12-19)

--- Assets/03. Scripts/05. MapEvent/DeadBodyEvent.cs
+++ Assets/03. Scripts/05. MapEvent/DeadBodyEvent.cs
@@ -8,10 +8,12 @@ namespace LeeJungChul
     public class DeadBodyEvent :Puzzle
 {
         public GameObject BloodEffect;
+        public GameObject BloodHand;
 
         public override void Interact()
         {
             BloodEffect.SetActive(true);
+            BloodHand.SetActive(true);
         }
     }
 }
