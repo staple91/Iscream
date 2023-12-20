a1ce8309f185c770cd67b77100c1e765c7e80813
staple91 <a998666@naver.com>
2023-12-20 20:47:24 +0900

    거울반사수정

--- Assets/03. Scripts/DirectRayLight.cs
+++ Assets/03. Scripts/DirectRayLight.cs
@@ -57,6 +57,10 @@ namespace No
         void ShotRay(RaycastHit hit, out bool isHit, ref Vector3 dir)
         {
             if(hit.transform.gameObject.layer ==  1 >> LayerMask.NameToLayer("RayDest"))
+            {
+                onSolved.Invoke();
+                Destroy(this);
+            }
 
 
             vectors.Add(hit.point);
